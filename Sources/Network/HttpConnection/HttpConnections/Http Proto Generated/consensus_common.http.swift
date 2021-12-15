0f039c0563528ccfda0912e5c9e6fc3828e36f60
Adam Mork <adam@mobilecoin.com>
2021-12-15 15:01:40 -0800

    remove unused imports in the HTTP clients

--- Sources/Network/HttpConnection/HttpConnections/Http Proto Generated/consensus_common.http.swift
+++ Sources/Network/HttpConnection/HttpConnections/Http Proto Generated/consensus_common.http.swift
@@ -4,7 +4,6 @@
 //  swiftlint:disable all
 
 import Foundation
-import NIO
 import SwiftProtobuf
 import LibMobileCoin
 
--- Sources/Network/HttpConnection/HttpConnections/Http Proto Generated/report.http.swift
+++ Sources/Network/HttpConnection/HttpConnections/Http Proto Generated/report.http.swift
@@ -21,7 +21,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 //
-import NIO
+
 import SwiftProtobuf
 import LibMobileCoin
 
