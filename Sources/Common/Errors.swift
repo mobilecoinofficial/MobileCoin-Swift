e699b7471ee507f7b33b1e76103663e609a6eb21
Cary A. Bakker <cary@scientificode.com>
2022-02-18 18:06:04 -0500

    Removed new error that's no longer used after refactoring code

--- Sources/Common/Errors.swift
+++ Sources/Common/Errors.swift
@@ -6,20 +6,6 @@
 
 import Foundation
 
-public struct UnexpectedError: Error {
-    let reason: String
-
-    init(_ reason: String) {
-        self.reason = reason
-    }
-}
-
-extension UnexpectedError: CustomStringConvertible {
-    public var description: String {
-        "Unexpected error: \(reason)"
-    }
-}
-
 public struct InvalidInputError: Error {
     let reason: String
 
