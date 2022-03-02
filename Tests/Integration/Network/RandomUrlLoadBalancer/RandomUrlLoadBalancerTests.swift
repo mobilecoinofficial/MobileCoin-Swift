79431eebf8013d72a6ece91d247e69599f838548
Cary A. Bakker <cary@scientificode.com>
2022-03-02 13:13:08 -0500

    Correct grpc test to use grpc instead of http

--- Tests/Integration/Network/RandomUrlLoadBalancer/RandomUrlLoadBalancerTests.swift
+++ Tests/Integration/Network/RandomUrlLoadBalancer/RandomUrlLoadBalancerTests.swift
@@ -21,19 +21,19 @@ class RandomUrlLoadBalancerIntTests: XCTestCase {
     }
 
     func verifyUrlRotates(transportProtocol: TransportProtocol) throws {
-        let client = try IntegrationTestFixtures.createMobileCoinClientWithPartialValidFogUrls(transportProtocol:TransportProtocol.http)
+        let client = try IntegrationTestFixtures.createMobileCoinClientWithPartialValidFogUrls(transportProtocol:transportProtocol)
 
         // using 10 iterations to be sure to get past all the incremental
         // failures from each of the individual services that may be used
         // during the balance query
         for _ in 0...10 {
             let expect = expectation(description: description)
-            try balance(client: client, transportProtocol: TransportProtocol.http, expectation: expect)
+            try balance(client: client, expectation: expect)
             waitForExpectations(timeout: 100)
         }
     }
 
-    func balance(client: MobileCoinClient, transportProtocol: TransportProtocol, expectation expect: XCTestExpectation) throws {
+    func balance(client: MobileCoinClient, expectation expect: XCTestExpectation) throws {
 
         client.updateBalance {
             guard $0.successOrFulfill(expectation: expect) != nil else { return }
