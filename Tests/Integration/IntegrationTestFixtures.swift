7b43f6f94fce393b26540658927e747f912f9ab5
the-real-adammork <adammork@gmail.com>
2022-04-11 14:33:26 -0700

    remove duplicate static var

--- Tests/Integration/IntegrationTestFixtures.swift
+++ Tests/Integration/IntegrationTestFixtures.swift
@@ -16,8 +16,6 @@ extension IntegrationTestFixtures {
     static let invalidFogUrl = "fog://invalid.mobilecoin.com"
     
     static let fee = McConstants.DEFAULT_MINIMUM_FEE
-    static let invalidConsensusUrl = "mc://invalid.mobilecoin.com"
-    static let invalidFogUrl = "fog://invalid.mobilecoin.com"
 
     static func fogReportUrlTyped() throws -> FogUrl {
         try FogUrl.make(string: network.fogReportUrl).get()
