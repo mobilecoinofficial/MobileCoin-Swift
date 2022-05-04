2860e41318d3b02d6e805e17ccc0fd783ccf328c
the-real-adammork <adammork@gmail.com>
2022-05-04 13:02:25 -0700

    update artifacts to latest (now old) v1.2.0, integration tests would need to run on alpha for this change (for now)

--- Tests/Integration/IntegrationTestFixtures.swift
+++ Tests/Integration/IntegrationTestFixtures.swift
@@ -8,7 +8,7 @@
 import XCTest
 
 enum IntegrationTestFixtures {
-    static let network: NetworkPreset = .mobiledev
+    static let network: NetworkPreset = .alpha
 }
 
 extension IntegrationTestFixtures {
