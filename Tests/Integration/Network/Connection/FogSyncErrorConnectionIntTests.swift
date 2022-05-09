e87aec02568d49cf7b4aad8d2b0d759b1c4efc67
the-real-adammork <adammork@gmail.com>
2022-05-08 23:28:25 -0700

    remove duplicate class

--- Tests/Integration/Network/Connection/FogSyncErrorConnectionIntTests.swift
+++ Tests/Integration/Network/Connection/FogSyncErrorConnectionIntTests.swift
@@ -244,36 +244,3 @@ class FogSyncErrorConnectionIntTests: XCTestCase {
     }
     
 }
-
-class MockFogSyncChecker: FogSyncCheckable {
-    var viewsHighestKnownBlock: UInt64
-    var ledgersHighestKnownBlock: UInt64
-    var consensusHighestKnownBlock: UInt64
-
-    let maxAllowedBlockDelta: PositiveUInt64
-    
-    static let delta: UInt64 = 10
-    
-    init(viewIndex: UInt64, ledgerIndex: UInt64, consensusIndex: UInt64) {
-        viewsHighestKnownBlock = viewIndex
-        ledgersHighestKnownBlock = ledgerIndex
-        consensusHighestKnownBlock = consensusIndex
-        
-        guard let positiveDelta = PositiveUInt64(Self.delta) else {
-            logger.fatalError("Should never be reached as 10 > 0")
-        }
-        maxAllowedBlockDelta = positiveDelta
-    }
-    
-    func setViewsHighestKnownBlock(_: UInt64) {
-        // Do nothing
-    }
-    
-    func setLedgersHighestKnownBlock(_: UInt64) {
-        // Do nothing
-    }
-    
-    func setConsensusHighestKnownBlock(_: UInt64) {
-        // Do nothing
-    }
-}
