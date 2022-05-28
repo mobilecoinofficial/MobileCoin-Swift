576914fc1a12dbc443497cb98de5736525a54507
the-real-adammork <adammork@gmail.com>
2022-05-28 16:33:47 -0700

    move data utils to new file

--- Sources/Ledger/PartialTxOut.swift
+++ Sources/Ledger/PartialTxOut.swift
@@ -88,31 +88,3 @@ extension PartialTxOut {
         }
     }
 }
-
-// TODO find better home
-extension Data {
-    init<T>(from value: T) {
-        self = Swift.withUnsafeBytes(of: value) { Data($0) }
-    }
-
-    func to<T>(type: T.Type) -> T? where T: ExpressibleByIntegerLiteral {
-        var value: T = 0
-        guard count >= MemoryLayout.size(ofValue: value) else { return nil }
-        _ = Swift.withUnsafeMutableBytes(of: &value, { copyBytes(to: $0)} )
-        return value
-    }
-    
-    func toUInt64() -> UInt64? {
-        switch self.count {
-        case 0:
-            return 0
-        case 4:
-            guard let value = self.to(type: UInt32.self) else { return nil }
-            return UInt64(value)
-        case 8:
-            return self.to(type: UInt64.self)
-        default:
-            return nil
-        }
-    }
-}
---
+++ Sources/Utils/Data/Data+TypeConversions.swift
@@ -0,0 +1,5 @@
+//
+//  Copyright (c) 2020-2021 MobileCoin. All rights reserved.
+//
+
+import Foundation
