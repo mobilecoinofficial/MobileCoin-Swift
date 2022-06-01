794b897f285e854d00044fcc2d33f311d8185ff9
the-real-adammork <adammork@gmail.com>
2022-06-01 02:10:24 -0700

    fix linting issues

--- Sources/Network/ProtocolSpecific/GRPCOnly/WrappedSSLCertificateValidator.swift
+++ Sources/Network/ProtocolSpecific/GRPCOnly/WrappedSSLCertificateValidator.swift
@@ -5,7 +5,11 @@
 import Foundation
 
 class WrappedSSLCertificateValidator: SSLCertificateValidator {
-    func validate(_ possibleCertificateData: [Data]) -> Result<PossibleSSLCertificates, InvalidInputError> {
-        .failure(InvalidInputError("Standard SSLCertificate validation not supported with GRPC only target"))
+    func validate(
+        _ possibleCertificateData: [Data]
+    ) -> Result<PossibleSSLCertificates, InvalidInputError> {
+        .failure(InvalidInputError(
+                    "Standard SSLCertificate validation " +
+                    "not supported with GRPC only target"))
     }
 }
