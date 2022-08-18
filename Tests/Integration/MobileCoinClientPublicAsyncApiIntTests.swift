0d7b2827afc1c0dcb32dafec0fde5a094c8a4c6f
Phil Larson <phil@mobilecoin.com>
2022-08-17 19:18:57 -0700

    Swiftlint

--- Tests/Integration/MobileCoinClientPublicAsyncApiIntTests.swift
+++ Tests/Integration/MobileCoinClientPublicAsyncApiIntTests.swift
@@ -1,15 +1,15 @@
 //
 //  Copyright (c) 2020-2021 MobileCoin. All rights reserved.
 //
+// swiftlint:disable superfluous_disable_command
 // swiftlint:disable type_body_length
 // swiftlint:disable file_length
+// swiftlint:disable empty_xctest_method
 
 import MobileCoin
 import XCTest
 
 #if swift(>=5.5)
-// swiftlint:disable superfluous_disable_command
-// swiftlint:disable empty_xctest_method
 
 @available(iOS 13.0, *)
 class MobileCoinClientPublicAsyncApiIntTests: XCTestCase {
