484a7612c6abfdee0391b2443d0ad4fdfa2bcdb1
Adam Mork <adam@mobilecoin.com>
2022-11-04 19:47:11 -0700

    linting

--- Sources/Account/PublicAddress.swift
+++ Sources/Account/PublicAddress.swift
@@ -57,7 +57,7 @@ public struct PublicAddress {
     public var spendPublicKey: Data { spendPublicKeyTyped.data }
 
     public var fogReportUrlString: String? { fogInfo?.reportUrlString }
-    
+
     public var addressHash: String? { calculateAddressHash()?.hex }
 
     var fogReportUrl: FogUrl? { fogInfo?.reportUrl }
