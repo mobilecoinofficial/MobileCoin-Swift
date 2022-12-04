3a35ad3974539748599a91c1bfeeb2b51ca0ec0d
Adam Mork <adam@mobilecoin.com>
2022-12-03 23:04:30 -0800

    add tests for new intent memo types and new request destination memo

--- Tests/Unit/Transaction/TxOutMemoIntegrationTests.swift
+++ Tests/Unit/Transaction/TxOutMemoIntegrationTests.swift
@@ -99,7 +99,7 @@ class TxOutMemoIntegrationTests: XCTestCase {
         let sentTxOut = fixture.sentTxOut
 
         guard
-            case let .destination(recoverable) = sentTxOut.recoverableMemo,
+            case let .destinationWithPaymentRequest(recoverable) = sentTxOut.recoverableMemo,
             let recovered = recoverable.recover()
         else {
             XCTFail("Unable to recover memo data")
@@ -109,6 +109,61 @@ class TxOutMemoIntegrationTests: XCTestCase {
         XCTAssertEqual(recovered.addressHash, recipientPublicAddress.calculateAddressHash())
         XCTAssertEqual(recovered.fee, fixture.fee.value)
         XCTAssertEqual(recovered.totalOutlay, fixture.totalOutlay)
+        XCTAssertEqual(recovered.paymentRequestId, fixture.paymentRequestId)
+    }
+
+    func testBuildTransactionWithSenderWithPaymentIntent() throws {
+        let fixture = try TransactionBuilder.Fixtures.SenderWithPaymentIntentAndDestination()
+        let txFixture = fixture.txFixture
+        let paymentIntentId = fixture.paymentIntentId
+
+        XCTAssertSuccess(TransactionBuilder.build(
+            inputs: txFixture.inputs,
+            accountKey: txFixture.senderAccountKey,
+            to: txFixture.recipientAccountKey.publicAddress,
+            memoType: fixture.memoType,
+            amount: txFixture.amount,
+            fee: txFixture.fee,
+            tombstoneBlockIndex: txFixture.tombstoneBlockIndex,
+            fogResolver: txFixture.fogResolver,
+            blockVersion: txFixture.blockVersion,
+            rngSeed: testRngSeed()))
+    }
+
+    func testTransactionWithSenderWithPaymentIntentMemo() throws {
+        let fixture = try TransactionBuilder.Fixtures.SenderWithPaymentIntentAndDestination()
+        let senderPublicAddress = fixture.senderPublicAddress
+        let receivedTxOut = fixture.receivedTxOut
+
+        guard
+            case let .senderWithPaymentIntent(recoverable) = receivedTxOut.recoverableMemo,
+            let recovered = recoverable.recover(senderPublicAddress: senderPublicAddress)
+        else {
+            XCTFail("Unable to recover memo data")
+            return
+        }
+
+        XCTAssertEqual(recovered.addressHash, senderPublicAddress.calculateAddressHash())
+        XCTAssertEqual(recovered.paymentIntentId, fixture.paymentIntentId)
+    }
+
+    func testTransactionSenderWithPaymentIntentDestinationMemo() throws {
+        let fixture = try TransactionBuilder.Fixtures.SenderWithPaymentIntentAndDestination()
+        let recipientPublicAddress = fixture.recipeintPublicAddress
+        let sentTxOut = fixture.sentTxOut
+
+        guard
+            case let .destinationWithPaymentIntent(recoverable) = sentTxOut.recoverableMemo,
+            let recovered = recoverable.recover()
+        else {
+            XCTFail("Unable to recover memo data")
+            return
+        }
+
+        XCTAssertEqual(recovered.addressHash, recipientPublicAddress.calculateAddressHash())
+        XCTAssertEqual(recovered.fee, fixture.fee.value)
+        XCTAssertEqual(recovered.totalOutlay, fixture.totalOutlay)
+        XCTAssertEqual(recovered.paymentIntentId, fixture.paymentIntentId)
     }
 
     func testBlockVersionOneUnusedMemo() throws {
