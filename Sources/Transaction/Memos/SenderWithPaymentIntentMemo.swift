db8820b629d8f744442b0371b7f10800d63aac3a
Adam Mork <adam@mobilecoin.com>
2022-12-15 20:37:11 -0800

    change Memo encoding to include typeBytes, typeName and "data" for Memo data, update unit tests, remove typeBytes enum add as static vars to memo types

--- Sources/Transaction/Memos/SenderWithPaymentIntentMemo.swift
+++ Sources/Transaction/Memos/SenderWithPaymentIntentMemo.swift
@@ -17,14 +17,24 @@ extension SenderWithPaymentIntentMemo: Equatable, Hashable { }
 
 extension SenderWithPaymentIntentMemo: Encodable {
     enum CodingKeys: String, CodingKey {
+        case typeBytes
+        case typeName
+        case data
+    }
+
+    enum DataCodingKeys: String, CodingKey {
         case addressHashHex
         case paymentIntentId
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(addressHashHex, forKey: .addressHashHex)
-        try container.encode(paymentIntentId, forKey: .paymentIntentId)
+        try container.encode(Self.type, forKey: .typeBytes)
+        try container.encode(Self.typeName, forKey: .typeName)
+
+        var data = container.nestedContainer(keyedBy: DataCodingKeys.self, forKey: .data)
+        try data.encode(addressHashHex, forKey: .addressHashHex)
+        try data.encode(String(paymentIntentId), forKey: .paymentIntentId)
     }
 }
 
--- Sources/Transaction/Memos/SenderWithPaymentRequestMemo.swift
+++ Sources/Transaction/Memos/SenderWithPaymentRequestMemo.swift
@@ -17,14 +17,24 @@ extension SenderWithPaymentRequestMemo: Equatable, Hashable { }
 
 extension SenderWithPaymentRequestMemo: Encodable {
     enum CodingKeys: String, CodingKey {
+        case typeBytes
+        case typeName
+        case data
+    }
+
+    enum DataCodingKeys: String, CodingKey {
         case addressHashHex
         case paymentRequestId
     }
 
     public func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
-        try container.encode(addressHashHex, forKey: .addressHashHex)
-        try container.encode(paymentRequestId, forKey: .paymentRequestId)
+        try container.encode(Self.type, forKey: .typeBytes)
+        try container.encode(Self.typeName, forKey: .typeName)
+
+        var data = container.nestedContainer(keyedBy: DataCodingKeys.self, forKey: .data)
+        try data.encode(addressHashHex, forKey: .addressHashHex)
+        try data.encode(String(paymentRequestId), forKey: .paymentRequestId)
     }
 }
 
