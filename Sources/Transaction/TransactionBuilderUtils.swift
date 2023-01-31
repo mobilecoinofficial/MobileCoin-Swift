5e89f59813c67fc016b3668a58b3991c48d8c0dd
the-real-adammork <adammork@gmail.com>
2023-01-31 12:50:14 -0800

    change _unmixed to existing, add _mixed to new mixed versions

--- Sources/Transaction/TransactionBuilderUtils.swift
+++ Sources/Transaction/TransactionBuilderUtils.swift
@@ -55,7 +55,7 @@ enum TransactionBuilderUtils {
             confirmationNumberData.asMcMutableBuffer { confirmationNumberPtr in
                 sharedSecretData.asMcMutableBuffer { sharedSecretPtr in
                     Data.make(withMcDataBytes: { errorPtr in
-                        mc_transaction_builder_add_output_unmixed(
+                        mc_transaction_builder_add_output(
                             ptr,
                             amount,
                             publicAddressPtr,
@@ -124,7 +124,7 @@ enum TransactionBuilderUtils {
                 confirmationNumberData.asMcMutableBuffer { confirmationNumberPtr in
                     sharedSecretData.asMcMutableBuffer { sharedSecretPtr in
                         Data.make(withMcDataBytes: { errorPtr in
-                            mc_transaction_builder_add_change_output_unmixed(
+                            mc_transaction_builder_add_change_output(
                                 accountKeyPtr,
                                 ptr,
                                 amount,
