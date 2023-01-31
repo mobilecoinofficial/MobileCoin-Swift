e170085ab6593eb02121a0f7a1d25ce0e6803118
Adam Mork <adam@mobilecoin.com>
2023-01-31 01:29:55 -0800

    wip

--- Tests/Integration/MobileCoinClientInternalIntTests.swift
+++ Tests/Integration/MobileCoinClientInternalIntTests.swift
@@ -4,14 +4,14 @@
 
 // swiftlint:disable all
 
-import MobileCoin
+@testable import MobileCoin
 import XCTest
 
 class MobileCoinClientInternalIntTests: XCTestCase {
 
     func testDefragmentationTesting() throws {
         let description = "Defragmentation Testing"
-        try testSupportedProtocols(description: description) {
+        try testSupportedProtocols(description: description, timeout: 1000) {
             try defragmentationTesting(transportProtocol: $0, expectation: $1)
         }
     }
@@ -28,35 +28,33 @@ class MobileCoinClientInternalIntTests: XCTestCase {
             var numChecksRemaining = 5
             func checkBalanceChange() {
                 numChecksRemaining -= 1
-                print("Updating balance...")
+                print("Defrage updating balance...")
                 client.updateBalances {
                     guard let balances = $0.successOrFulfill(expectation: expect) else { return }
                     print("Balances: \(balances)")
                         
-                    let diff: UInt64 = {
-                        do {
-                            let balancesMap = balances.balances
-                            let balancesBeforeMap = balancesBefore.balances
-                            let mobUSD = try XCTUnwrap(balancesMap[TokenId.MOBUSD]?.amount())
-                            let initialMobUSD = try XCTUnwrap(balancesBeforeMap[.MOBUSD]?.amount())
-                            
-                            guard mobUSD != initialMobUSD else {
-                                guard numChecksRemaining > 0 else {
-                                    XCTFail("Failed to receive a changed balance. initial balance: " +
-                                            "\(initialMobUSD), current balance: " +
-                                            "\(mobUSD) microMOBUSD")
-                                    expect.fulfill()
-                                    return
-                                }
-                                
-                                Thread.sleep(forTimeInterval: 2)
-                                checkBalanceChange()
+                    var diff: UInt64 = 0
+                    do {
+                        let balancesMap = balances.balances
+                        let balancesBeforeMap = balancesBefore.balances
+                        let mob = balancesMap[TokenId.MOB]?.amount() ?? 0
+                        let initialMob = try XCTUnwrap(balancesBeforeMap[.MOB]?.amount())
+                        
+                        guard mob != initialMob else {
+                            guard numChecksRemaining > 0 else {
+                                XCTFail("Failed to receive a changed balance. initial balance: " +
+                                        "\(initialMob), current balance: " +
+                                        "\(mob) picoMOB")
+                                expect.fulfill()
                                 return
                             }
-                            return max(mobUSD, initialMobUSD)
-                                .subtractingReportingOverflow(min(mobUSD, initialMobUSD)).partialValue
-                        } catch {}
-                    }()
+                            
+                            Thread.sleep(forTimeInterval: 2)
+                            checkBalanceChange()
+                            return
+                        }
+                        diff = 9
+                    } catch {}
                     completion(.success(diff))
                 }
             }
@@ -86,117 +84,153 @@ class MobileCoinClientInternalIntTests: XCTestCase {
             defragee: MobileCoinClient,
             defrager: MobileCoinClient,
             amount: UInt64,
+            count: Int,
+            index: Int,
             completion: @escaping (Result<(), TransactionSubmissionError>) -> Void
         ) {
-            Array(repeating: 0, count: splitFactor).map { _ in
-                amount.dividedReportingOverflow(by: UInt64(splitFactor)).partialValue
+            guard index != count else {
+                completion(.success(()))
+                return
             }
-            .mapAsync({ fragmentAmount, callback in
-                defrager.prepareTransaction(
-                    to: defrageeAccount.publicAddress,
-                    amount: fragmentAmount,
-                    fee: IntegrationTestFixtures.fee
-                ) {
-                    guard let (transaction, _) = $0.successOrFulfill(expectation: expect)
-                    else { return }
-                    
-                    let defragerBalances = defrager.balances
-                    defrager.submitTransaction(transaction) {
-                        guard $0.successOrFulfill(expectation: expect) != nil else { return }
-                        
-                        print("Transaction submission successful")
-                        
-                        verifyBalanceChange(defrager, defragerBalances, completion: callback)
-                    }
-                }
-            },
-            serialQueue: serialQueue,
-            completion: { diffs in
-                print("diffs from having the defrager fragment the account \(diffs)")
-                
-                // verify that account 5 needs to be defragged to send (amount/20)*17
-                // defrag account 5
-                // verify that account 5 does not need to be defragged to send (amount/20)*17
-            })
-
-        }
-
-        try IntegrationTestFixtures.createMobileCoinClientWithBalance(
-                accountIndex: defragerIndex,
-                expectation: expect,
-                transportProtocol: transportProtocol)
-        { defragerClient in
-            try? IntegrationTestFixtures.createMobileCoinClientWithBalance(
-                accountIndex: defrageeIndex,
-                expectation: expect,
-                transportProtocol: transportProtocol)
-            { defrageeClient in
-                defrageeClient.amountTransferable(tokenId: .MOB, completion: { result in
-                    guard let fullAmount = try? result.get() else {
-                        XCTFail("Unable to calculate full amount")
-                        return
-                    }
-                    
-                    print("Full amount available in the defragee account \(fullAmount)")
-                    
-                    defrageeClient.prepareTransaction(
-                        to: defragerAccount.publicAddress,
-                        amount: fullAmount,
-                        fee: IntegrationTestFixtures.fee
-                    ) {
-                        guard let (transaction, _) = $0.successOrFulfill(expectation: expect)
-                        else { return }
-                        
-                        let defragerBalances = defragerClient.balances
-                        let defrageeBalances = defrageeClient.balances
-                        defrageeClient.submitTransaction(transaction) {
-                            guard $0.successOrFulfill(expectation: expect) != nil else { return }
-                            
-                            print("Transaction submission successful")
-                        
-                            verifyBalanceChange(defragerClient, defragerBalances, completion: { _ in
-                                verifyBalanceChange(defrageeClient, defrageeBalances, completion: { _ in
-                                    fragmentAccount(
-                                        defragee: defrageeClient,
-                                        defrager: defragerClient,
-                                        amount: fullAmount
-                                    ) { _ in
-                                        
-                                    }
-                                })
-                            })
-                        }
-                    }
-                })
-            }
-        }
-
-        
             
-        let recipient = try IntegrationTestFixtures.createPublicAddress(accountIndex: 1)
-
-        try IntegrationTestFixtures.createMobileCoinClientWithBalance(
-                expectation: expect,
-                transportProtocol: transportProtocol)
-        { client in
-            client.prepareTransaction(
-                to: recipient,
-                amount: 100,
+            let fragmentAmount = amount.dividedReportingOverflow(by: UInt64(splitFactor)).partialValue
+            
+            print("Defrag transaction preperation")
+            defrager.prepareTransaction(
+                to: defrageeAccount.publicAddress,
+                amount: fragmentAmount,
                 fee: IntegrationTestFixtures.fee
             ) {
                 guard let (transaction, _) = $0.successOrFulfill(expectation: expect)
                 else { return }
-
-                print("transaction fixture: \(transaction.serializedData.hexEncodedString())")
-
-                client.submitTransaction(transaction) {
+                
+                print("Defrag transaction preperation successful")
+                let defragerBalances = defrager.balances
+                defrager.submitTransaction(transaction) {
                     guard $0.successOrFulfill(expectation: expect) != nil else { return }
-
-                    print("Transaction submission successful")
-                    expect.fulfill()
+                    
+                    print("Defrag transaction submission successful")
+                    
+                    verifyBalanceChange(defrager, defragerBalances) { _ in
+                        defrager.updateBalances() { _ in
+                            fragmentAccount(
+                                defragee: defragee,
+                                defrager: defrager,
+                                amount: amount,
+                                count: count,
+                                index: (index + 1),
+                                completion: completion
+                            )
+                        }
+                    }
                 }
             }
         }
-    }
 
+        func loadUpDefragerAccount(
+            defrageeClient: MobileCoinClient,
+            defragerClient: MobileCoinClient,
+            fullAmount: UInt64,
+            completion: @escaping () -> Void
+        ) {
+            defrageeClient.prepareTransaction(
+                to: defragerAccount.publicAddress,
+                amount: fullAmount,
+                fee: IntegrationTestFixtures.fee
+            ) {
+                guard let (transaction, _) = $0.successOrFulfill(expectation: expect)
+                else { return }
+                
+                let defragerBalances = defragerClient.balances
+                let defrageeBalances = defrageeClient.balances
+                defrageeClient.submitTransaction(transaction) {
+                    guard $0.successOrFulfill(expectation: expect) != nil else { return }
+                    
+                    print("Defrag transaction submission successful")
+                    
+                    verifyBalanceChange(defragerClient, defragerBalances, completion: { _ in
+                        verifyBalanceChange(defrageeClient, defrageeBalances, completion: { _ in
+                            completion()
+                        })
+                    })
+                }
+            }
+        }
+        
+        func createBothClients(
+            completion: @escaping (_ defragee: MobileCoinClient, _ defrager: MobileCoinClient) -> Void
+        ) {
+            do {
+                try IntegrationTestFixtures.createMobileCoinClientWithBalance(
+                    accountIndex: defragerIndex,
+                    expectation: expect,
+                    transportProtocol: transportProtocol)
+                { defragerClient in
+                    try? IntegrationTestFixtures.createMobileCoinClientWithBalance(
+                        accountIndex: defrageeIndex,
+                        expectation: expect,
+                        transportProtocol: transportProtocol)
+                    { defrageeClient in
+                        completion(defrageeClient, defragerClient)
+                    }
+                }
+            } catch {
+                XCTFail("Defrag, unable to create defrager account")
+            }
+        }
+        
+        func prepareForLoadingDefrager(
+            defrageeClient: MobileCoinClient,
+            defragerClient: MobileCoinClient,
+            completion: @escaping (_ defragee: MobileCoinClient, _ defrager: MobileCoinClient, _ fullAmount: UInt64) -> Void
+        ) {
+            defrageeClient.amountTransferable(tokenId: .MOB, completion: { result in
+                guard let fullAmount = try? result.get() else {
+                    XCTFail("Defrag unable to calculate full amount")
+                    return
+                }
+                
+                print("Defrag full amount available in the defragee account \(fullAmount)")
+                
+                defrageeClient.requiresDefragmentation(toSendAmount: fullAmount) { result in
+                    guard let defragRequired = try? result.get() else {
+                        XCTFail("Defrag unable to calculate full amount")
+                        return
+                    }
+                    
+                    if defragRequired {
+                        defrageeClient.prepareDefragmentationStepTransactions(
+                            toSendAmount: fullAmount
+                        ) { result in
+                            guard let stepTransactions = try? result.get() else {
+                                XCTFail("Defrag unable to calculate defrag step transactions")
+                                return
+                            }
+                            
+                            // Defrag account then call completion
+                            
+                        }
+                    } else {
+                        // Defrag call completion
+                    }
+                }
+            }
+        }
+        
+            
+//                loadUpDefragerAccount(defrageeClient: defrageeClient, defragerClient: defragerClient) { _ in
+//                    fragmentAccount(
+//                        defragee: defrageeClient,
+//                        defrager: defragerClient,
+//                        amount: fullAmount,
+//                        count: splitFactor,
+//                        index: 0
+//                    ) { _ in
+//                        // verify that account 5 needs to be defragged to send (amount/20)*17
+//                        // defrag account 5
+//                        // verify that account 5 does not need to be defragged to send (amount/20)*17
+//                        expect.fulfill()
+//                    }
+//                }
+    }
 }
