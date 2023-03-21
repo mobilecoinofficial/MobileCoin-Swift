4f6f4554634a11a5adc7fd10b18cfd427fabd665
Cary A. Bakker <carybakker@users.noreply.github.com>
2023-03-21 14:43:04 -0400

    Cary/dynamic test wallets 184577127 (#242)
    * Dynamic test account creation - Ability to create any desired number of test accounts, with configurable transaction history / multiple txos of any desired token id + amount configuration
    * Integration test that creates 10 test wallets with various transaction combinations, validating the transaction history, and returning all funds to the source account
    * NOTE: Test is disabled - it is not meant to be a ‘regular’ integration test, it’s only meant for one-off manual validation of the test account creation so we don’t burn through fees unnecessarily
    * OTHER NOTE: We’ll likely not go forward with returning dust.  After Slack convo with Henry, it seems that recovery of the fee dust is difficult / not worth it for blocks with single or few transactions (like testNet).  A better approach is to move our integration tests to lean on the use of MOBUSD (aka eUSD) as we can mint more.

---
+++ Tests/Util/TestAccountFactory.swift
@@ -0,0 +1,117 @@
+//
+//  Copyright (c) 2020-2023 MobileCoin. All rights reserved.
+//
+@testable import MobileCoin
+import XCTest
+
+#if swift(>=5.5)
+
+@available(iOS 15.0, *)
+
+public struct TestAccountFactory {
+
+    enum TestAccountFactoryError: Error {
+        case unknownError(_ description: String)
+    }
+
+    let fogReportUrl: String
+    let fogReportId: String = ""
+    let fogAuthoritySpki: Data
+    let accountIndex: UInt32 = 0
+
+    public struct TestAccountConfig {
+        let name: String
+        let txAmounts: [Amount]
+
+        init(name: String, txAmounts: [Amount]) {
+            self.name = name
+            self.txAmounts = txAmounts
+        }
+
+        init(name: String, txData: [TokenId: [UInt64]]) {
+            self.name = name
+            var amounts = [Amount]()
+            for tokenId in txData.keys {
+                guard let vals = txData[tokenId] else { continue }
+                for val in vals {
+                    amounts.append(Amount(val, in: tokenId))
+                }
+            }
+            self.txAmounts = amounts
+        }
+    }
+
+    public struct TestAccount {
+        var name: String { config.name }
+        let accountKey: AccountKey
+        let config: TestAccountConfig
+    }
+
+    func makeAccounts(
+        sourceClient: MobileCoinClient,
+        testAccountConfigs: [TestAccountConfig]
+    ) async throws -> [TestAccount] {
+
+        var testAccounts = [TestAccount]()
+        for config in testAccountConfigs {
+            let acct = try await self.createNewFundedAccount(
+                sourceClient: sourceClient,
+                testAccountConfig: config)
+            testAccounts.append(acct)
+        }
+
+        return testAccounts
+    }
+
+    func createNewFundedAccount(
+        sourceClient: MobileCoinClient,
+        testAccountConfig: TestAccountConfig
+    ) async throws -> TestAccount {
+        guard let entropy32 = Data32(.secRngGenBytes(32)) else {
+            fatalError(".secRngGenBytes(32) should always create a valid Data32")
+        }
+
+        print("New Account: \(entropy32.base64EncodedString())")
+
+        let acctKey = try AccountKey.make(
+            rootEntropy: entropy32.data,
+            fogReportUrl: self.fogReportUrl,
+            fogReportId: self.fogReportId,
+            fogAuthoritySpki: self.fogAuthoritySpki)
+            .get()
+
+        for amount in testAccountConfig.txAmounts {
+            let fee = try await sourceClient.estimateTotalFee(
+                toSendAmount: amount,
+                feeLevel: .minimum)
+
+            let transaction = try await sourceClient.prepareTransaction(
+                to: acctKey.publicAddress,
+                amount: amount,
+                fee: fee)
+
+            try await sourceClient.submitTransaction(
+                transaction: transaction.transaction)
+
+            var txComplete = false
+            while !txComplete {
+                try await sourceClient.updateBalances()
+                let txStatus = try await sourceClient.status(of: transaction.transaction)
+                switch txStatus {
+                case .accepted(block: _ ):
+                    txComplete = true
+                case .failed:
+                    throw TestAccountFactoryError.unknownError("fund tx failed")
+                case .unknown:
+                    // throttle the polling a bit
+                    sleep(1)
+                }
+            }
+        }
+
+        return TestAccount(accountKey: acctKey, config: testAccountConfig)
+    }
+
+}
+
+#endif
