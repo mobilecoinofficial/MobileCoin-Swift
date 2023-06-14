f7617433bbd3f8bee76b301497e09817e3e6b00d
the-real-adammork <adammork@gmail.com>
2023-06-13 19:38:56 -0700

    process_info generation working

--- tools/generate_process_info_jsons.sh
+++ tools/generate_process_info_jsons.sh
@@ -11,6 +11,15 @@ check_dependencies -exit_on_install > /dev/null
 
 TEST_ACCOUNT_SEED="$(security find-generic-password -w -s swift-repo-public | head -c32 | base64)"
 
+# test account seed will be 32 bytes of your contribution key
 jq --null-input \
   --arg TEST_ACCOUNT_SEED "$TEST_ACCOUNT_SEED" \
   '{ "testAccountSeed": $TEST_ACCOUNT_SEED }' > $REPO_ROOT/Tests/Common/Secrets/process_info.json
+
+# we need the src account entropy for the next process_info.json
+source <(../scripts/decrypt_secrets)
+
+jq --null-input \
+  --arg TEST_ACCOUNT_SEED "$TEST_ACCOUNT_SEED" \
+  --arg SRC_ACCT_ENTROPY_STRING "$SRC_ACCT_ENTROPY_STRING" \
+  '{ "testAccountSeed": $TEST_ACCOUNT_SEED, "srcAcctEntropyString": $SRC_ACCT_ENTROPY_STRING }' > $REPO_ROOT/tools/TestSetupClient/TestSetupClientTests/process_info.json
