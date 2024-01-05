ddc4d8bca0ab8c8f749cee8abacc2a4f0f167435
Adam Mork <adammork@gmail.com>
2024-01-04 17:33:09 -0800

    feat: add advisory appears to be working, and new create call that accepts advisories on create. next: do same for signer, write tests

--- Sources/Common/Network/Attestation/AttestationVerifier.swift
+++ Sources/Common/Network/Attestation/AttestationVerifier.swift
@@ -60,7 +60,7 @@ private final class MrEnclaveVerifier {
         
         self.ptr = mrEnclave.mrEnclave.asMcBuffer { mrEnclavePtr in
             // Safety: mc_mr_enclave_verifier_create should never fail.
-            withMcInfallible { mc_trusted_identity_mr_enclave_create(mrEnclavePtr) }
+            withMcInfallible { mc_trusted_identity_mr_enclave_create(mrEnclavePtr, configAdvisories, hardeningAdvisories) }
         }
 
 //        mrEnclave.allowedConfigAdvisories.forEach(addConfigAdvisory)
